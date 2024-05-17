db0142c7b5ddd41ee7102018ded7d7ab4a5664ae
alma8026 <alikswtf@gmail.com>
2024-05-17 19:28:46 +0200

    portfolio with funcitonal links

--- portfolio_angular/src/app/app.component.ts
+++ portfolio_angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
