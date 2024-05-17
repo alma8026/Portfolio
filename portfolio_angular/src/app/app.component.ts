6347bc12e4e0f319a81003481a298e58de3dff7c
alma8026 <alikswtf@gmail.com>
2024-05-17 18:33:35 +0200

    plantilla portfolio done

--- portfolio_angular/src/app/app.component.ts
+++ portfolio_angular/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio_angular';
+  
+  mobileMenuOpen: boolean = false;
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
 }
