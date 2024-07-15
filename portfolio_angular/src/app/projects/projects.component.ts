2c00cc7eba4a361a3fa33f44c5604f3af30e7a04
alma8026 <alikswtf@gmail.com>
2024-07-15 18:49:39 +0200

    projects updated, added carrousel

--- portfolio_angular/src/app/projects/projects.component.ts
+++ portfolio_angular/src/app/projects/projects.component.ts
@@ -13,11 +13,52 @@ import { HttpClient } from '@angular/common/http';
 export class ProjectsComponent {
 
   constructor() {}
-
+  
   formData: any = {}
 
-  submitForm() {
-    
+  submitForm() {}
+
+  // Definición de proyectos con sus datos
+  project1 = {
+    currentImage: 'assets/images/aura_page1.png', // Imagen inicial
+    images: [
+      'assets/images/aura_page1.png',
+      'assets/images/aura_page2.png',
+      'assets/images/aura_page3.png'
+    ],
+    currentIndex: 0 // Índice de la imagen actual
+  };
+
+  project2 = {
+    currentImage: 'assets/images/PokemonCards1.PNG', // Imagen inicial
+    images: [
+      'assets/images/PokemonCards1.PNG',
+      'assets/images/PokemonCards2.PNG',
+      'assets/images/PokemonCards3.PNG'
+    ],
+    currentIndex: 0 // Índice de la imagen actual
+  };
+
+  project3 = {
+    currentImage: 'assets/images/E-learn1.PNG', // Imagen inicial
+    images: [
+      'assets/images/E-learn1.PNG',
+      'assets/images/E-learn2.PNG',
+      'assets/images/E-learn3.PNG'
+    ],
+    currentIndex: 0 // Índice de la imagen actual
+  };
+
+  // Función para cambiar a la imagen anterior
+  prevImage(project: any) {
+    project.currentIndex = (project.currentIndex - 1 + project.images.length) % project.images.length;
+    project.currentImage = project.images[project.currentIndex];
+  }
+
+  // Función para cambiar a la siguiente imagen
+  nextImage(project: any) {
+    project.currentIndex = (project.currentIndex + 1) % project.images.length;
+    project.currentImage = project.images[project.currentIndex];
   }
   
 }
