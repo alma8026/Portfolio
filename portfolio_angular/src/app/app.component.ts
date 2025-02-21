5664b58491b4bada9814c205ee59437cd9bd4a94
alma8026 <alikswtf@gmail.com>
2025-02-21 03:11:06 +0100

    starting experience

--- portfolio_angular/src/app/app.component.ts
+++ portfolio_angular/src/app/app.component.ts
@@ -7,11 +7,12 @@ import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HomeComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent],
+  imports: [RouterOutlet, CommonModule, HomeComponent, AboutComponent, ExperienceComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
